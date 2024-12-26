5a4747b8919554e74fa3c3a56b3db75dba527588
jupiterbjy <jupiterbjy@e8ight.co.kr>
2024-12-26 15:57:54 +0900

    Added build/wheel dependencies

---
+++ build.sh
@@ -0,0 +1,1 @@
+pip wheel --no-deps .
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+Setup script for Tigris API Client
+"""
+
+from setuptools import setup, find_packages
+import re
+
+# from pytigris import __version__ as pytigris_version
+
+
+_VERSION_RE = re.compile(r"__version__ = ['\"]([^'\"]+)['\"]")
+
+setup(
+    name="pytigris",
+    version=_VERSION_RE.search(open("pytigris/__init__.py").read()).group(1),
+    author="jupiterbjy",
+    author_email="jupiterbjy@gmail.com",
+    description="Unofficial Tigris API Client",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jupiterbjy/pytigris",
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=open("requirements.txt").read().splitlines(),
+    extras_require={"trio": ["trio"]},
+)
